736b6410c9301409b4ebbb8ca73d625e64288043
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-06-04 20:59:20 +0800

    change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-// import logo from './logo.svg'
-// import './App.css'
 import About from './components/About'
 import Info from './components/Info'
 import Footer from './components/Footer'
 import Skills from './components/Skills'
 
 function App() {
-  //   return (
-  //     <div className='App'>
-  //       <Info />
-  //       <About />
-  //       <Skills />
-  //       <Footer />
-  //     </div>
-  //   )
-  // }
-
   return (
     <div className='card--container'>
       <div className='card'>
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react'
-// import ReactDOM from 'react-dom/client'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Popper from '@popperjs/core'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
